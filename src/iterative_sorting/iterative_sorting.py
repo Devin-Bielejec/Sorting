09a053bcca13487ad0f46158dc2bfc7c78bcb587
Devin Bielejec <devin.bielejec@gmail.com>
2019-11-27 18:40:33 -0500

    Select sort complete

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -12,20 +12,20 @@
     
     c. When the correct index is found, copy temp into this position
 """
-def swap(a, b, li):
-    temp = a
-    a = b
-    b = temp
-    return a, b
+def swap(index1, index2, li):
+    temp = li[index1]
+    li[index1] = li[index2]
+    li[index2] = temp
+    return li
+
 
 def insertionSort(A):
-    for index1 in list(range(1, len(A))):
-        for index2 in list(range(index1-1, -1, -1)):
+    for index1 in range(1, len(A)):
+        for index2 in range(index1-1, -1, -1):
             if A[index1] < A[index2]:
                 #swap items
-                temp = A[index1]
-                A[index1] = A[index2]
-                A[index2] = temp
+                swap(index1, index2, A)
+                #Reduce index1 by 1 because it has moved to the left
                 index1 -= 1
             else:
                 break 
@@ -49,22 +49,23 @@ print(insertionSort([5,3,2,1]))
 
 
 # TO-DO: Complete the selection_sort() function below 
-def selection_sort(arr):
-    # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
-        for j in range(i, len(arr)):
-            if (arr[j] < arr[smallest_index]):
-                smallest_index = j 
+def selection_sort(A):
+    #Loop through all but last element
+    for index1 in range(len(A)-1):
+        print(A)
+        print(A[index1])
+        indexMin = index1
+        for index2 in range(index1, len(A)):
+            if A[index2] < A[indexMin]:
+                indexMin = index2
+        #Swap current elemtn with smallest element
+        if A[index1] > A[indexMin]:
+            swap(index1, indexMin, A) 
+    return A
 
-        # TO-DO: swap
-        temp = arr[cur_index]
-        arr[cur_index] = arr[smallest_index]
-        arr[smallest_index] = temp
-    return arr
+
+
+print(selection_sort([14, 6, 3, 5, 32, -1, -2, 4, 10 , 2]))
 
 """
 ### Algorithm
@@ -74,7 +75,6 @@ def selection_sort(arr):
 2. If no swaps performed, stop. Else, go back to the element at index 0 and repeat step 1.
 """
 
-print(selection_sort([10,9,8, -1, 15, 3, 2]))
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
