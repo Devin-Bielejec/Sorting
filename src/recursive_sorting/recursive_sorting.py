4df6668bd8b07121946d253ea4a08a7113d6b6a7
Devin Bielejec <devin.bielejec@gmail.com>
2019-11-28 12:53:57 -0500

    Merge sort complete

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -14,14 +14,28 @@ def merge( arrA, arrB ):
             arrB.pop(0)
     return merged_arr
 
+"""
+1. While your data set contains more than one item, split it in half
+2. Once you have gotten down to a single element, you have also *sorted* that element 
+   (a single element cannot be "out of order")
+3. Start merging your single lists of one element together into larger, sorted sets
+4. Repeat step 3 until the entire data set has been reassembled
+"""
+
 print(merge([1,5,6], [7,8,10]))
+
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
+    if len(arr) == 1:
+        return arr
+    else: 
+        left = arr[:len(arr)//2]
+        right = arr[len(arr)//2:]
+        return merge(merge_sort(left),merge_sort(right))
+        
 
-    return arr
-
-
+print(merge_sort([4,9,3,1,5,6]))
 # STRETCH: implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
     # TO-DO
